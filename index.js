a35e9a7bba12e61de9a2edfad648d7dbf452f2d4
APNpandit611 <APNpanditt@gmail.com>
2024-01-11 11:11:02 +0200

    all files for portfolio

---
+++ index.js
@@ -0,0 +1,29 @@
+import express, { urlencoded } from "express";
+import bodyParser from "body-parser"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended:true }));
+
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/about", (req, res) =>{
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}.`);
+});
