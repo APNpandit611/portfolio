3b6996771fd849b3de9350762f7a6340edec86d6
APNpandit611 <apnpanditt@gmail.com>
2024-10-25 15:02:42 +0300

    ready to build about page

--- src/components/banner/Button.js
+++ src/components/banner/Button.js
@@ -5,7 +5,7 @@ const Button = ({ buttonName }) => {
         <>
             <button
                 type="submit"
-                className="hover:text-red-500 cursor-pointer duration-300 ease mx-3 mt-5 decoration-solid bg-slate-800 text-slate-300 shadow-2xl shadow-indigo-500/40 py-2 px-5 rounded-lg hover:-translate-y-0.5 "
+                className=" hover:bg-indigo-500 hover:text-slate-300 cursor-pointer duration-300 ease mx-3 mt-5 decoration-solid bg-slate-800 text-slate-300 shadow-2xl shadow-indigo-500/40 py-2 px-5 rounded-lg hover:-translate-y-0.5 "
             >
                 {buttonName}
             </button>
--- src/components/footer/Social.js
+++ src/components/footer/Social.js
@@ -9,10 +9,10 @@ import {
 const Social1 = () => {
     return (
         <div className=" fixed bottom-0 left-10">
-            <div className="text-slate-300 flex-col mt-8 hover:text-red-500 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaSquareGithub size={20} /></div>
-            <div className="text-slate-300 flex-col mt-8 hover:text-red-500 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaSquareFacebook size={20}/></div>
-            <div className="text-slate-300 flex-col mt-8 hover:text-red-500 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaSquareInstagram size={20} /></div>
-            <div className="text-slate-300 flex-col mt-8 mb-8 hover:text-red-500 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaLinkedin   size={20} /></div>
+            <div className="text-slate-300 flex-col mt-8 hover:text-neutral-950 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaSquareGithub size={20} /></div>
+            <div className="text-slate-300 flex-col mt-8 hover:text-blue-800 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaSquareFacebook size={20}/></div>
+            <div className="text-slate-300 flex-col mt-8 hover:text-rose-900 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaSquareInstagram size={20} /></div>
+            <div className="text-slate-300 flex-col mt-8 mb-8 hover:text-blue-500 cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5"><FaLinkedin   size={20} /></div>
             <div className="relative h-[200px] w-[4px] bottom-0 bg-slate-300 left-2"></div>
         </div>
     );
