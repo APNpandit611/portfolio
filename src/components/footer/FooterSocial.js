e6fd4ee1b7a80d7f33293d2bbb41eae00eeeaaf2
APNpandit611 <apnpanditt@gmail.com>
2024-12-02 13:22:22 +0200

    done

--- src/components/footer/FooterSocial.js
+++ src/components/footer/FooterSocial.js
@@ -8,18 +8,26 @@ import {
 const FooterSocial = () => {
     return (
         <div className="flex gap-x-10">
-            <div className="text-slate-300 flex-col hover:text-black cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
-                <FaSquareGithub size={24} />
-            </div>
-            <div className="text-slate-300 flex-col hover:text-[#1877F2] cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
-                <FaSquareFacebook size={24} />
-            </div>
-            <div className="text-slate-300 flex-col hover:text-[#E4405F] cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
-                <FaSquareInstagram size={24} />
-            </div>
-            <div className="text-slate-300 flex-col hover:text-[#0077B5] cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
-                <FaLinkedin size={24} />
-            </div>
+            <a href="https://github.com/APNpandit611">
+                <div className="text-slate-300 flex-col hover:text-black cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
+                    <FaSquareGithub size={24} />
+                </div>
+            </a>
+            <a href="https://www.facebook.com/Abhi9821290007/1">
+                <div className="text-slate-300 flex-col hover:text-[#1877F2] cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
+                    <FaSquareFacebook size={24} />
+                </div>
+            </a>
+            <a href="https://www.instagram.com/disboyhigh/">
+                <div className="text-slate-300 flex-col hover:text-[#E4405F] cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
+                    <FaSquareInstagram size={24} />
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/abhishek-pandit-02544415a">
+                <div className="text-slate-300 flex-col hover:text-[#0077B5] cursor-pointer duration-300 ease transition transform hover:-translate-y-0.5">
+                    <FaLinkedin size={24} />
+                </div>
+            </a>
         </div>
     );
 };
