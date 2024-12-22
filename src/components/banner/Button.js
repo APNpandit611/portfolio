46a4ec25e22b0fce3857b38872588f4d84b448be
APNpandit611 <apnpanditt@gmail.com>
2024-12-22 18:18:32 +0200

    banner button fixed

--- src/components/banner/Button.js
+++ src/components/banner/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ buttonName }) => {
     return (
         <>
-            <a href="../asset">
+            <a href="https://drive.google.com/file/d/14TlZOzBYr-u908n_r8uzvNkngiUZehkk/view?usp=drive_link">
                 <button
                     type="submit"
                     className=" hover:bg-indigo-500 hover:text-slate-300 cursor-pointer duration-300 ease mx-3 mt-5 decoration-solid bg-slate-800 text-slate-300 shadow-2xl shadow-indigo-500/40 py-2 px-5 rounded-lg hover:-translate-y-0.5 "
